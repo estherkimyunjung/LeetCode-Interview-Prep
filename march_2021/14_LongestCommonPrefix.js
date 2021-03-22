ab160f9a9a662f9b1edc7ca7f024bad90137c2e9
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-21 23:38:08 -0500

    2nd solution prefix

--- march_2021/14_LongestCommonPrefix.js
+++ march_2021/14_LongestCommonPrefix.js
@@ -1,32 +1,41 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
 
-const longestCommonPrefix = (strs) => {
-  if (strs.length == 0) return console.log("no exsit prefix");
+// const longestCommonPrefix = (strs) => {
+//   if (strs.length == 0) return console.log("no exsit prefix");
 
-  var final = strs[0];
-  var min = strs[0].length;
-  for (var i = 0; i < strs.length; i++) {
-    if (strs[i].length < min) {
-      min = strs[i].length;
-      final = strs[i];
-    }
-  }
+//   var final = strs[0];
+//   var min = strs[0].length;
+//   for (var i = 0; i < strs.length; i++) {
+//     if (strs[i].length < min) {
+//       min = strs[i].length;
+//       final = strs[i];
+//     }
+//   }
 
-  for (var i = final.length; i >= 0; i--) {
-    var flag = true;
-    var val = final.substring(0, i);
-    for (var j = 0; j < strs.length; j++) {
-      if (strs[j].substring(0, i) != val) {
-        flag = false;
-        break;
-      }
-    }
-    if (flag == true) {
-      return console.log(val);
-      break;
-    }
-  }
+//   for (var i = final.length; i >= 0; i--) {
+//     var flag = true;
+//     var val = final.substring(0, i);
+//     for (var j = 0; j < strs.length; j++) {
+//       if (strs[j].substring(0, i) != val) {
+//         flag = false;
+//         break;
+//       }
+//     }
+//     if (flag == true) {
+//       return console.log(val);
+//       break;
+//     }
+//   }
+// };
+
+const longestCommonPrefix = (str, prefix = "", j = 0) => {
+  if (!str.length) return "";
+
+  while (str[0][j] && str.every((_, i) => str[0][j] === str[i][j]))
+    prefix += str[0][j++];
+
+  return console.log(prefix);
 };
 
 longestCommonPrefix(["flower", "flow", "flight"]);
