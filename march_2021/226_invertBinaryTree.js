16c60c496376ca2f827347071abb657cb95d8eeb
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-22 19:11:35 -0500

    invert binary tree

---
+++ march_2021/226_invertBinaryTree.js
@@ -0,0 +1,33 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+const invertTree = (root) => {
+  if (root) {
+    invertTree(root.left);
+    invertTree(root.right);
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+  }
+  return root;
+};
+
+// const invertTree = (root) => {
+//     let head = root;
+
+//     let queue = new Array;
+//     queue.push(root);
+
+//     while(queue.length > 0){
+//         let node = queue.shift();
+
+//         if(node){
+//             queue.push(node.left);
+//             queue.push(node.right);
+//             let temp = node.left;
+//             node.left = node.right;
+//             node.right = temp;
+//         }
+//     }
+//     return head;
+// };
