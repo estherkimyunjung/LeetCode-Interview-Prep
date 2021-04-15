a53e179ac0d230518393a8be8d5b8a208f26523c
estherkimyunjung <estherkimyunjung@gmail.com>
2021-04-14 22:49:15 -0500

    binary tree depth

---
+++ april_2021/543_DiameterOfBinaryTree.js
@@ -0,0 +1,22 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const diamterOfBinaryTree = function (root) {
+  let maxDepth = 0;
+
+  const depth = (node) => {
+    let left = depth(node.left);
+    let right = depth(node.right);
+
+    maxDepth = Math.max(maxDepth, left + right);
+    return left + right + 1;
+  };
+
+  depth(root);
+  return console.log(maxDepth);
+};
+
+diamterOfBinaryTree([1, 2, 3, 4, 5]);
