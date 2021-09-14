5e3b9427185e66f84226388052bb8f51b8f031b0
estherkimyunjung <estherkimyunjung@gmail.com>
2021-09-13 23:02:47 -0500

    link list

---
+++ september_2021/86_PartitionList.js
@@ -0,0 +1,33 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+const partition = (head, x) => {
+  if (head === null) return head;
+  let dummyHead = new ListNode(0, head);
+  let beforePointer = dummyHead;
+  // find the first node with val greater or equal then x
+  // point to one node before
+  while (beforePointer.next.val < x) {
+    beforePointer = beforePointer.next;
+    // if reaching the end, then simply return
+    if (!beforePointer.next) return head;
+  }
+
+  // scan the rest and find node val less than x
+  let preHead = beforePointer.next;
+  while (preHead.next) {
+    let head = preHead.next;
+    if (head.val >= x) {
+      preHead = head;
+      continue;
+    }
+    // move the node only needed
+    preHead.next = head.next;
+    head.next = beforePointer.next;
+    beforePointer.next = head;
+    beforePointer = head;
+  }
+  return dummyHead.next;
+};
