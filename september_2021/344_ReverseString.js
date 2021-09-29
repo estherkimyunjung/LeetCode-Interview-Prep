9ae1f1dda04033d42ccc2d1489b5ed8c2fe71afd
estherkimyunjung <estherkimyunjung@gmail.com>
2021-09-28 19:41:18 -0500

    reverse strig  solve by recursion

---
+++ september_2021/344_ReverseString.js
@@ -0,0 +1,15 @@
+var reverseString = function (s) {
+  reverse(s, 0, s.length - 1);
+};
+
+function reverse(s, beginIndex, endIndex) {
+  if (beginIndex >= endIndex) {
+    return;
+  }
+
+  const first = s[beginIndex];
+  const last = s[endIndex];
+  s[beginIndex] = last;
+  s[endIndex] = first;
+  reverse(s, beginIndex + 1, endIndex - 1);
+}
