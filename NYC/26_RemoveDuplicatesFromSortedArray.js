54b2856b72f86e4ac25b244f7151df90a60ec34d
estherkimyunjung <estherkimyunjung@gmail.com>
2021-10-17 18:53:56 -0500

    added RemoveDuplicatesFromSortedArray.js

---
+++ NYC/26_RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,46 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+// Return k after placing the final result in the first k slots of nums.
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Custom Judge:
+// The judge will test your solution with the following code:
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+// int k = removeDuplicates(nums); // Calls your implementation
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+// array -> f -> array
+
+// const removeDuplicates = (nums) => {
+//   let k = nums.length;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1]) {
+//       nums[i + 1] = null;
+//       if (nums[i + 1] === null)
+//         [nums[i + 1], nums[i + 2]] = [nums[i + 2], nums[i + 1]];
+//       i++;
+//       k--;
+//     } else {
+//       i++;
+//     }
+//   }
+//   return console.log(nums);
+// };
+
+const removeDuplicates = (nums) => {
+  nums.forEach((element, i) => {
+    if (element === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      removeDuplicates(nums);
+    }
+  });
+  return console.log(nums);
+};
+
+removeDuplicates([1, 1, 2]);
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
