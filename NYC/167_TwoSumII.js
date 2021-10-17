f7d40c3551c721dda667a7fe56403b20c0d4a23e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-10-17 07:32:27 -0500

    added twoSumII.js

---
+++ NYC/167_TwoSumII.js
@@ -0,0 +1,39 @@
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+// find two numbers such that they add up to a specific target number.
+// Let these two numbers be numbers[index1] and numbers[index2]
+// where 1 <= first < second <= numbers.length.
+
+// Return the indices of the two numbers, index1 and index2, as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Constraints:
+// 2 <= numbers.length <= 3 * 104
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
+
+// 1. array -> f -> return array of two numbers
+// 2. ask contraints - input array is sorted in increasing,
+// exactly one solution,
+// return index(index start at 1)
+
+const twoSum = (numbers, target) => {
+  let firstPointer = 0,
+    secondPointer = numbers.length - 1;
+
+  while (firstPointer < secondPointer) {
+    if (numbers[firstPointer] + numbers[secondPointer] === target) {
+      console.log([firstPointer + 1, secondPointer + 1]);
+      return [firstPointer + 1, secondPointer + 1];
+    } else if (numbers[firstPointer] + numbers[secondPointer] > target) {
+      secondPointer--;
+    } else {
+      firstPointer++;
+    }
+  }
+};
+
+twoSum([2, 7, 11, 15], 9);
+twoSum([2, 3, 4], 6);
